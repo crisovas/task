b8dfd04fe07eb35a5be63c8dd8f4b5e13850a560
Chris Samuel <Chris Samuel@DESKTOP-U0V2R58>
2020-06-05 22:22:00 +0100

    second task

---
+++ src/tasktwo/TaskTwo.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tasktwo;
+
+/**
+ *
+ * @author Chris Samuel
+ */
+public class TaskTwo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String firstName;
+        String lastName;
+        int HNGi7Id;
+          
+          firstName = "Christian";
+          lastName = "Samuel";
+          HNGi7Id = 2000;
+          
+          System.out.print("Hello World, this is");
+          System.out.print(" "+ firstName);
+          System.out.println(" "+ lastName);
+          System.out.println("ID "+HNGi7Id);
+        
+    }
+    
+}
